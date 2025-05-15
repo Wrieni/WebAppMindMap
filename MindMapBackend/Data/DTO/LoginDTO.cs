23393e880a30a5939f65eac47658edcc7a7be586
Wrieni <divanniyexpert76@gmail.com>
2025-05-15 14:07:01 +0300

    add lodin & register DTO

---
+++ MindMapBackend/Data/DTO/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace MindMapBackend.Data.DTO
+{
+    public class LoginDTO
+    {
+        public required string email { get; set; }
+        public required string password { get; set; }
+    }
+}
---
+++ MindMapBackend/Data/DTO/RegisterDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MindMapBackend.Data.DTO
+{
+    public class RegisterDTO
+    {
+        public string name { get; set; } = null!;
+
+        public string surname { get; set; } = null!;
+
+        public string email { get; set; } = null!;
+
+        public string password { get; set; } = null!;
+
+    }
+}
